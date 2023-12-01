c890f1033c6c5c7d6a4a2012d44de991ade197a3
Diogo Novo <diogo.a.novo@gmail.com>
2023-12-01 22:09:44 +0000

    Day01 solutions added.

--- Advent-of-Code-2023/src/Day01.kt
+++ Advent-of-Code-2023/src/Day01.kt
@@ -1,18 +1,79 @@
+private const val AOC_DAY = "Day01"
+private const val TEST_FILE = "${AOC_DAY}_test"
+private const val INPUT_FILE = AOC_DAY
+
+
+private fun part1(input: List<String>): Int {
+    return input.sumOf { line ->
+        line.first { it.isDigit() }.digitToInt() * 10 + line.last { it.isDigit() }.digitToInt()
+    }
+}
+
+
+
+private val digitsLetters = listOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+private data class CalibrationValue(val index: Int, val value: Int)
+fun part2(input: List<String>): Int {
+    return input.sumOf { line ->
+        var firstVal = CalibrationValue(Int.MAX_VALUE, 0)
+        var lastVal = CalibrationValue(Int.MIN_VALUE, 0)
+
+        for ((i, digit) in digitsLetters.withIndex()) {
+            if (digit in line) {
+                val firstIdx = line.indexOf(digit)
+                val lastIdx = line.lastIndexOf(digit)
+                if (firstIdx < firstVal.index) {
+                    firstVal = CalibrationValue(firstIdx, i + 1)
+                }
+                if (lastIdx > lastVal.index) {
+                    lastVal = CalibrationValue(lastIdx, i + 1)
+                }
+            }
+        }
+
+        val firstIdx = line.indexOfFirst { it.isDigit() }
+        val lastIdx = line.indexOfLast { it.isDigit() }
+        if (firstIdx >= 0 && firstIdx < firstVal.index) {
+            firstVal = CalibrationValue(firstIdx, line[firstIdx].digitToInt())
+        }
+        if (lastIdx >= 0 && lastIdx > lastVal.index) {
+            lastVal = CalibrationValue(lastIdx, line[lastIdx].digitToInt())
+        }
+
+        firstVal.value * 10 + lastVal.value
+    }
+}
+
+
+private fun getNumber(line: String, digitsLetters: List<String>): Int {
+    val (letterIdx, letterValue) = line.findAnyOf(digitsLetters) ?: (Int.MAX_VALUE to "")
+    val digitIdx = line.indexOfFirst { it.isDigit() }
+
+    return if (digitIdx < 0 || letterIdx < digitIdx) {
+        digitsLetters.indexOf(letterValue) + 1
+    } else {
+        line[digitIdx].digitToInt()
+    }
+}
+
+
+/**
+ * Instead of searching from the end of each line, by reversing the line and the digits' letters, it searches for
+ * the last number in the same order but with the line reversed: eno, owt, eerht, ruof, ...
+ */
+private fun part2Improved(input: List<String>): Int {
+    return input.sumOf { line ->
+        getNumber(line, digitsLetters) * 10 + getNumber(line.reversed(), digitsLetters.map { it.reversed() })
+    }
+}
+
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        "boas".println()
-        return input.size
-    }
+    val testInput = readInputToList(TEST_FILE)
+    part2(testInput).println()
 
-    fun part2(input: List<String>): Int {
-        "boas".println()
-        return input.size
-    }
-
-    val testInput = readInputToList("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInputToList("Day01")
+    val input = readInputToList(INPUT_FILE)
     part1(input).println()
     part2(input).println()
+    part2Improved(input).println()
 }
--- Advent-of-Code-2023/src/Utils.kt
+++ Advent-of-Code-2023/src/Utils.kt
@@ -1,5 +1,3 @@
-import java.math.BigInteger
-import java.security.MessageDigest
 import kotlin.io.path.Path
 import kotlin.io.path.readLines
 import kotlin.io.path.readText
@@ -7,20 +5,12 @@ import kotlin.io.path.readText
 /**
  * Reads lines from the given input txt file into a list of strings.
  */
-fun readInputToList(name: String) = Path("src/$name.txt").readLines() //.readText()
+fun readInputToList(name: String) = Path("src/$name.txt").readLines()
 
 /**
  * Reads lines from the given input txt file into a single string.
  */
 fun readInputToString(name: String) = Path("src/$name.txt").readText()
-
-/**
- * Converts string to md5 hash.
- * - It's an example of how to create extension functions
- */
-fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
-    .toString(16)
-    .padStart(32, '0')
 
 /**
  * The cleaner shorthand for printing output.
