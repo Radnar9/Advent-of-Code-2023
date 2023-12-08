a7f011fed6b96678ed9dc62bd00e9d0cf0f371a4
Diogo Novo <diogo.a.novo@gmail.com>
2023-12-08 10:47:51 +0000

    Day08 solutions added.

---
+++ Advent-of-Code-2023/src/Day08.kt
@@ -0,0 +1,79 @@
+private const val AOC_DAY = "Day08"
+private const val PART1_TEST_FILE = "${AOC_DAY}_test_part1"
+private const val PART2_TEST_FILE = "${AOC_DAY}_test_part2"
+private const val INPUT_FILE = AOC_DAY
+
+private data class Direction(val left: String, val right: String)
+
+/**
+ * Counts the number of steps until finding a position ending with "ZZZ".
+ */
+private fun part1(input: List<String>): Int {
+    var instructions = input.first()
+    val map = input.drop(2).associate {
+        val line = it.split(" = ")
+        val dir = line[1].removePrefix("(").removeSuffix(")").split(", ")
+        line[0] to Direction(dir[0], dir[1])
+    }
+
+    var currentDir = "AAA"
+    var steps = 0
+    while (currentDir != "ZZZ") {
+        currentDir = if (instructions[0] == 'R') map[currentDir]!!.right else map[currentDir]!!.left
+        instructions = instructions.substring(1) + instructions[0]
+        steps++
+    }
+
+    return steps
+}
+
+private const val STARTING_CHAR = 'A'
+private const val ENDING_CHAR = 'Z'
+
+/**
+ * After finding that the numbers of steps until finding a position ending with 'Z' is always the same for each initial
+ * position, I just need to find each cycle length and calculate the Least Common Multiple of all of them.
+ */
+private fun part2(input: List<String>): Long {
+    var instructions = input.first()
+    val startingNodes = mutableListOf<String>()
+    val map = input.drop(2).associate {
+        val line = it.split(" = ")
+        if (line[0].last() == STARTING_CHAR) startingNodes.add(line[0])
+        val dir = line[1].removePrefix("(").removeSuffix(")").split(", ")
+        line[0] to Direction(dir[0], dir[1])
+    }
+
+    val cycles = mutableListOf<Long>()
+    for (i in startingNodes.indices) {
+        var steps = 0L
+        while (startingNodes[i].last() != ENDING_CHAR) {
+            val node = startingNodes[i]
+            startingNodes[i] = if (instructions[0] == 'R') map[node]!!.right else map[node]!!.left
+            instructions = instructions.substring(1) + instructions[0]
+            steps++
+        }
+        cycles.add(steps)
+    }
+    return lcm(cycles)
+}
+
+
+
+fun main() {
+    val part1TestInput = readInputToList(PART1_TEST_FILE)
+    val part1ExpectedRes = 6
+    println("---| TEST INPUT |---")
+    println("* PART 1:   ${part1(part1TestInput)}\t== $part1ExpectedRes")
+
+    val part2TestInput = readInputToList(PART2_TEST_FILE)
+    val part2ExpectedRes = 6
+    println("* PART 2:   ${part2(part2TestInput)}\t== $part2ExpectedRes\n")
+
+
+    val input = readInputToList(INPUT_FILE)
+    val improving = true
+    println("---| FINAL INPUT |---")
+    println("* PART 1: ${part1(input)}${if (improving) "\t== 11567" else ""}")
+    println("* PART 2: ${part2(input)}${if (improving) "\t== 9858474970153" else ""}")
+}
--- Advent-of-Code-2023/src/Utils.kt
+++ Advent-of-Code-2023/src/Utils.kt
@@ -1,6 +1,21 @@
 import kotlin.io.path.Path
+import kotlin.io.path.createFile
 import kotlin.io.path.readLines
 import kotlin.io.path.readText
+
+/**
+ * Creates the input test files for the given day.
+ */
+fun createTestFiles(day: String) {
+    try {
+        Path("src/$day.txt").createFile()
+        Path("src/${day}_test.txt").createFile()
+        Path("src/${day}_test_part1.txt").createFile()
+        Path("src/${day}_test_part2.txt").createFile()
+    } catch (e: Exception) {
+//        println("Test files already exist!")
+    }
+}
 
 /**
  * Reads lines from the given input txt file into a list of strings.
@@ -17,10 +32,35 @@ fun readInputToString(name: String) = Path("src/$name.txt").readText()
  */
 fun Any?.println() = println(this)
 
+/**
+ * Verifies if the [Char] is a symbol that is not a dot.
+ */
 fun Char.isSymbolNotDot(): Boolean {
     return !this.isLetterOrDigit() && this != '.'
 }
 
+/**
+ * Verifies if the [Char] is an asterisk.
+ */
 fun Char.isAsterisk(): Boolean {
     return this == '*'
+}
+
+/**
+ * Calculates the Greatest Common Divisor of two [Long] numbers.
+ */
+fun gcd(a: Long, b: Long): Long {
+    if (b == 0L) return a
+    return gcd(b, a % b)
+}
+
+/**
+ * Calculates the Least Common Multiple of a list of [Long] numbers.
+ */
+fun lcm(numbers: List<Long>): Long {
+    var lcm = numbers.first()
+    for (i in 1..<numbers.size) {
+        lcm = lcm * numbers[i] / gcd(lcm, numbers[i])
+    }
+    return lcm
 }
